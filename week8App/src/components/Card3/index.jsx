84c3f8ebd9761c2f107a49a1f3c54fa618b58d34
Martin老師 <tpemartin@gmail.com>
2023-05-03 12:20:15 +0800

    Update index.jsx

--- week8App/src/components/Card3/index.jsx
+++ week8App/src/components/Card3/index.jsx
@@ -6,24 +6,28 @@ import Image from 'mui-image'
 
 export default function Card({ user, key }) {
   const { name, grade, imgSrc, wish } = user
-  var FindFirst = function FindFirst() {
+  function FindFirst() {
     var first_name = name[0]
     return (first_name)
   }
   /*
-  var FindFirst = function(){....} or
+  Either 
+  var FindFirst = function(){....} 
+  or
   function FindFirst(){....} 
   */
 
   /*
   the return of FindFirst is a string
   a string can not be destructured like an object as
+  var [firstname ] = aArray // is okay
   var { firstname } = anObject // is okay
   var { firstname } = aString // is not okay
   The solution is to use instead
   var firstname = FindFirst() // is okay
   */
-  var { firstname } = FindFirst()
+  // var { firstname } = FindFirst()
+  var firstname = FindFirst()
   return (<>
     <div className="card3">
       <div className="card-content">
@@ -47,7 +51,7 @@ export default function Card({ user, key }) {
             <div className="sender-grade">{grade}</div>
             <div className="Avator"><Avatar
               sx={{ width: 68, height: 68, fontSize: 40 }}
-            >H</Avatar></div>
+            >{firstname}</Avatar></div>
           </div>
         </div>
       </div>
